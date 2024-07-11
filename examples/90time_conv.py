32c8a0782dde20b0c043546d28e008bf36c8e8c9
John Franck <jmfranck@jmfDebian.syr.edu>
2024-07-11 15:06:37 -0400

    fix pulse length conversion

--- examples/90time_conv.py
+++ examples/90time_conv.py
@@ -7,12 +7,14 @@ in order to get a pulse length with the actual
 desired pulse length.
 """
 import matplotlib.pyplot as plt
-from pyspecdata import *
+from numpy import r_
+import pyspecdata as psd
 from SpinCore_pp.pulse_length_conv import prog_plen
 
-desired_plen = r_[0:25:25j]
+# the following choice fails dramatically with the old code
+desired_plen = r_[0:50:50j]
 prog_plen = prog_plen(desired_plen)
-with figlist_var() as fl:
+with psd.figlist_var() as fl:
     fl.next("Actual vs programmed plen")
     plt.plot(prog_plen, desired_plen, "o")
     plt.ylabel(r"actual plen / $\mu$s")
