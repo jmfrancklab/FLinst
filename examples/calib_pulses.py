f5aec3fce412f785a2df46a580b8f6c9e9ea2efb
Alex Guinness <aguinnes@syr.edu>
2024-07-30 08:56:06 -0400

    add docstring

--- examples/calib_pulses.py
+++ examples/calib_pulses.py
@@ -1,3 +1,13 @@
+"""
+Calibrate pulses output from RF amplifier
+=========================================
+A series of betas ($\mu$s $\sqrt{W}$) are supplied to the spincore to output to
+the RF amplifier. The output of the amplifier is then captured on the GDS
+oscilloscope. It is very important to note that there MUST BE at least a 40 dBm
+attenuator between the RF amplifier output and the GDS oscilloscope input to
+avoid damaging the instrumentation! It is advised that the attenuator be
+calibrated using the GDS and AFG beforehand
+"""
 import pyspecdata as psd
 import os
 import SpinCore_pp as spc
