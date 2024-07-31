73e5d63c4f65b14dd4792ef88adcc0050966798b
Alexandria Guinness <aguinnes@syr.edu>
2024-07-31 15:08:14 -0400

    change fl next

--- examples/90time_conv.py
+++ examples/90time_conv.py
@@ -12,7 +12,7 @@ from SpinCore_pp.pulse_length_conv import prog_plen
 
 with psd.figlist_var() as fl:
     for amplitude in [(1.0), (0.5)]:
-        fl.next(r"desired $\beta$ vs pulse length")
+        fl.next(r"Pulse Length vs. Desired $\beta$")
         plt.plot(r_[10:150:50j], prog_plen(r_[10:150:50j], amplitude), "o")
         plt.xlabel(r"desired $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
         plt.ylabel(r"required pulse lengths / $\mathrm{\mu s}$")
