ae698986c6426cc98f58f4572cb037e4931c5528
John Franck <jmfranck@jmfDebian.syr.edu>
2024-07-31 15:35:38 -0400

    reviewed the calibration function

--- SpinCore_pp/pulse_length_conv.py
+++ SpinCore_pp/pulse_length_conv.py
@@ -3,54 +3,67 @@ import numpy as np
 
 def prog_plen(desired_beta, amplitude):
     """
-    Takes the desired beta (μs sqrt(W)) and tells the
-    user what pulse length should be programmed in order to get the desired
-    beta
+    Takes the desired β (μs√(W)) and tells the user
+    what pulse length should be programmed in order to
+    get the desired β
+
     Parameters
     ==========
     desired_beta: float
-                    the desired beta you wish the spincore to output,
-                    in μs*sqrt(W)
+        the desired β you wish the spincore to output,
+        in μs*sqrt(W)
     amplitude:  float
-                amplitude setting of measurement
+        amplitude setting of measurement
+
     Returns
     =======
     retval: float
-            The pulse length you tell spincore in order to get the desired beta.
+        The pulse length you tell spincore in order to
+        get the desired β.
     """
     # NOTE: THESE VALUES NEED TO BE RECALCULATED ONCE THE RERUN ACQUISITIONS ARE PROCESSED!
-    if amplitude > 0.5:
+    if amplitude == 1.0:
         linear_threshold = 60
         c_curve = [
-            1.60730341e02 + 0.0j,
-            -3.00153883e02 + 0.0j,
-            2.43406692e02 + 0.0j,
-            -1.08779978e02 + 0.0j,
-            3.10482870e01 + 0.0j,
-            -5.93976000e00 + 0.0j,
-            7.72557235e-01 + 0.0j,
-            -6.74756111e-02 + 0.0j,
-            3.78673112e-03 + 0.0j,
-            -1.23280806e-04 + 0.0j,
-            1.76812546e-06 + 0.0j,
+            1.60730341e02,
+            -3.00153883e02,
+            2.43406692e02,
+            -1.08779978e02,
+            3.10482870e01,
+            -5.93976000e00,
+            7.72557235e-01,
+            -6.74756111e-02,
+            3.78673112e-03,
+            -1.23280806e-04,
+            1.76812546e-06,
         ]
-        c_linear = [1.294623894 + 0.0j, -12349235 + 0.0j, 923569239 + 0.0j]
-    else:
+        c_linear = [
+            1.294623894,
+            -12349235,
+            923569239,
+        ]
+    elif amplitude == 0.1:
         linear_threshold = 25
         c_curve = [
-            2.27868899e03 + 0.0j,
-            -3.17948111e03 + 0.0j,
-            1.95216192e03 + 0.0j,
-            -6.93869836e02 + 0.0j,
-            1.58378940e02 + 0.0j,
-            -2.42789260e01 + 0.0j,
-            2.53532435e00 + 0.0j,
-            -1.78351870e-01 + 0.0j,
-            8.09953581e-03 + 0.0j,
-            -2.14653411e-04 + 0.0j,
-            2.52318231e-06 + 0.0j,
+            2.27868899e03,
+            -3.17948111e03,
+            1.95216192e03,
+            -6.93869836e02,
+            1.58378940e02,
+            -2.42789260e01,
+            2.53532435e00,
+            -1.78351870e-01,
+            8.09953581e-03,
+            -2.14653411e-04,
+            2.52318231e-06,
         ]
-        c_linear = [1.294623894 + 0.0j, -12349235 + 0.0j, 923569239 + 0.0j]
+        c_linear = [
+            1.294623894,
+            -12349235,
+            923569239,
+        ]
+    else:
+        raise ValueError("not currently calibrated for this amplitude!!!")
 
     # }}}
     def zonefit(desired_beta):
