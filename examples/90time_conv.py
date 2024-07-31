b17a103e4da43aae0e54468c50f11350bce90b0c
John Franck <jmfranck@jmfDebian.syr.edu>
2024-07-31 15:38:14 -0400

    reviewed demo file

--- examples/90time_conv.py
+++ examples/90time_conv.py
@@ -3,7 +3,7 @@
 The programmed SpinCore pulse length does not
 match the actual output pulse length. This example
 finds what pulse length should be fed to SpinCore
-in order to get the desired beta.
+in order to get the desired β.
 """
 import matplotlib.pyplot as plt
 from numpy import r_
@@ -11,9 +11,9 @@ import pyspecdata as psd
 from SpinCore_pp.pulse_length_conv import prog_plen
 
 with psd.figlist_var() as fl:
-    for amplitude in [(1.0), (0.5)]:
-        fl.next(r"Pulse Length vs. Desired $\beta$")
-        plt.plot(r_[10:150:50j], prog_plen(r_[10:150:50j], amplitude), "o")
-        plt.xlabel(r"desired $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
-        plt.ylabel(r"required pulse lengths / $\mathrm{\mu s}$")
-        plt.title(r"Pulse Length vs. Desired $\beta$")
+    fl.next(r"Pulse Length vs. Desired $\beta$", legend=True)
+    for amplitude in [1.0, 0.1]:
+        plt.plot(r_[0:150:100j], prog_plen(r_[10:150:50j], amplitude), "o",
+                 label=f"amp={amplitude}")
+    plt.xlabel(r"desired $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
+    plt.ylabel(r"required pulse lengths / $\mathrm{\mu s}$")
