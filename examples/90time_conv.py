b7fac8e588d6c6ffe210989ee7378485d2916d44
Alex Guinness <aguinnes@syr.edu>
2024-07-31 12:01:40 -0400

    fixed examplel

--- examples/90time_conv.py
+++ examples/90time_conv.py
@@ -10,12 +10,10 @@ from numpy import r_
 import pyspecdata as psd
 from SpinCore_pp.pulse_length_conv import prog_plen
 
-# the following choice fails dramatically with the old code
-desired_beta = r_[10:150:50j]
-prog_plen = prog_plen(desired_beta)
 with psd.figlist_var() as fl:
-    fl.next(r"desired $\beta$ vs pulse length")
-    plt.plot(desired_beta, prog_plen, "o")
-    plt.xlabel(r"desired $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
-    plt.ylabel(r"required pulse lengths / $\mathrm{\mu s}$")
-    plt.title(r"Pulse Length vs. Desired $\beta$")
+    for amplitude in [(1.0), (0.5)]:
+        fl.next(r"desired $\beta$ vs pulse length")
+        plt.plot(r_[10:150:50j], prog_plen(r_[10:150:50j], amplitude), "o")
+        plt.xlabel(r"desired $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
+        plt.ylabel(r"required pulse lengths / $\mathrm{\mu s}$")
+        plt.title(r"Pulse Length vs. Desired $\beta$")
