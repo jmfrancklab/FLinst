878fbf345293dbc98cff71e4d42418fcb35592c8
Alex Guinness <aguinnes@syr.edu>
2024-08-13 14:20:57 -0400

    add counter save for other PR

--- examples/run_onepulse_nutation.py
+++ examples/run_onepulse_nutation.py
@@ -32,6 +32,7 @@ config_dict = SpinCore_pp.configuration("active.ini")
 # {{{add file saving parameters to config dict
 config_dict["type"] = "FID_nutation"
 config_dict["date"] = datetime.now().strftime("%y%m%d")
+config_dict["FID_nutation_counter"] += 1
 # }}}
 # {{{ command-line option to leave the field untouched (if you set it once, why set it again)
 if len(sys.argv) == 2 and sys.argv[1] == "stayput":
