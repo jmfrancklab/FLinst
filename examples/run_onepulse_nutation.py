f6b5f74f61fdff6dfdf48293cdc994837fdce1cb
Alex Guinness <aguinnes@syr.edu>
2024-08-15 11:46:17 -0400

    move input statement

--- examples/run_onepulse_nutation.py
+++ examples/run_onepulse_nutation.py
@@ -35,13 +35,13 @@ config_dict["date"] = datetime.now().strftime("%y%m%d")
 config_dict["FID_nutation_counter"] += 1
 # }}}
 # {{{ command-line option to leave the field untouched (if you set it once, why set it again)
+input(
+    "I'm assuming that you've tuned your probe to %f since that's what's in your .ini file. Hit enter if this is true"
+    % config_dict["carrierFreq_MHz"]
+)
 if len(sys.argv) == 2 and sys.argv[1] == "stayput":
     pass
 else:
-    input(
-        "I'm assuming that you've tuned your probe to %f since that's what's in your .ini file. Hit enter if this is true"
-        % config_dict["carrierFreq_MHz"]
-    )
     spc.set_field(config_dict)
 # }}}
 # {{{set phase cycling
