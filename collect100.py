1caa0bc63a2439e9be94d15fc68ad591cfe98763
Alex Guinness <aguinnes@syr.edu>
2024-08-15 12:41:44 -0400

    master?

--- collect100.py
+++ collect100.py
@@ -1,4 +1,4 @@
-#{{{Program doc
+#{{{ Program doc
 r'''Use this program to collect 100 snapshots of noise in
     about 1 min. This is to generate the Power Spectral
     Density of the device/system. Set up for testing the
@@ -14,14 +14,15 @@ r'''Use this program to collect 100 snapshots of noise in
     These parameters were determined to be ideal for capturing
     noise on earliest version of spectrometer (using Probe v1.0)
     Note: Set Trigger (Menu) --> Mode --> Auto
-
-        *** want to have CH2 on as well as CH1 - both set to 50 mV
+    
     TO RUN: Type 'py collec100.py file_name' where file_name will be the string
     identifier which is associated with the output file. The file will be saved
     as YYMMDD_file_name.h5 following today's date.
 
+    *** want to have CH2 on as well as CH1 - both set to 50 mV
+
 '''
-# }}}
+#}}}
 from pylab import *
 from datetime import datetime
 from Instruments import *
@@ -30,72 +31,61 @@ import time
 from timeit import default_timer as timer
 from serial.tools.list_ports import comports
 import serial
-import Instruments as inst
-import numpy as np
-import sys
+from scipy import signal
 
 acquire = False
-fl = psd.figlist_var()
+fl = figlist_var()
 
-
-# print "These are the instruments available:"
-# SerialInstrument(None)
-# print "done printing available instruments"
+#print "These are the instruments available:"
+#SerialInstrument(None)
+#print "done printing available instruments"
 #
-# with SerialInstrument('GDS-3254') as s:
+#with SerialInstrument('GDS-3254') as s:
 #    print s.respond('*idn?')
-def collect(date, id_string, captures):
+def collect(date,id_string,captures):
     capture_length = len(captures)
     start = timer()
     datalist = []
     print("about to load GDS")
-    with inst.GDS_scope() as g:
+    with GDS_scope() as g:
         print("loaded GDS")
-        for x in range(1, capture_length + 1):
-            print("entering capture", x)
+        for x in range(1,capture_length+1):
+            print("entering capture",x)
             ch1_waveform = g.waveform(ch=1)
             print("GOT WAVEFORM")
-            data = psd.concat([ch1_waveform], "ch").reorder("t")
+            data = concat([ch1_waveform],'ch').reorder('t')
             if x == 1:
-                channels = (
-                    (data.shape) + ("capture", capture_length)
-                ).alloc(dtype=np.float64)
-                channels.setaxis("t", data.getaxis("t")).set_units("t", "s")
-                channels.setaxis("ch", data.getaxis("ch"))
-            channels["capture", x - 1] = data
+                channels = ((ndshape(data)) + ('capture',capture_length)).alloc(dtype=float64)
+                channels.setaxis('t',data.getaxis('t')).set_units('t','s')
+                channels.setaxis('ch',data.getaxis('ch'))
+            channels['capture',x-1] = data
             time.sleep(1)
-            # {{{ in case pulled from inactive channel
-            if not np.isfinite(data.getaxis("t")[0]):
+            #{{{ in case pulled from inactive channel
+            if not isfinite(data.getaxis('t')[0]):
                 j = 0
-                while not np.isfinite(data.getaxis("t")[0]):
-                    data.setaxis("t", datalist[j].getaxis("t"))
-                    j += 1
+                while not isfinite(data.getaxis('t')[0]):
+                    data.setaxis('t',datalist[j].getaxis('t'))
+                    j+=1
                     if j == len(datalist):
-                        raise ValueError(
-                            "None of the time axes returned by the scope are finite, which probably means no traces are active??"
-                        )
-            # }}}
+                        raise ValueError("None of the time axes returned by the scope are finite, which probably means no traces are active??")
+            #}}}
     s = channels
-    s.labels("capture", captures)
-    s.name("accumulated_" + date)
-    s.hdf5_write(
-        date + "_" + id_string + ".h5",
-        directory=psd.getDATADIR(exp_type="ODNP_NMR_comp/noise_tests"),
-    )
-    print("name of data", s.name())
-    print("units should be", s.get_units("t"))
-    print("shape of data", s.shape)
+    s.labels('capture',captures)
+    s.name('accumulated_'+date)
+    s.hdf5_write(date+'_'+id_string+'.h5',
+            directory=getDATADIR(exp_type='ODNP_NMR_comp/noise_tests'))
+    print("name of data",s.name())
+    print("units should be",s.get_units('t'))
+    print("shape of data",ndshape(s))
     return start
 
-
-date = datetime.now().strftime("%y%m%d")
-if len(sys.argv) < 2:
-    raise ValueError("give an experiment name on the command line!")
+date = datetime.now().strftime('%y%m%d')
+if len(sys.argv) < 2: raise ValueError("give an experiment name on the command line!")
 id_string = sys.argv[1]
-captures = np.linspace(1, 100, 100)
+captures = linspace(1,100,100)
 
 print("Starting collection...")
-start = collect(date, id_string, captures)
+start = collect(date,id_string,captures)
 end = timer()
 
-print("Collection time:", (end - start), "s")
+print("Collection time:",(end-start),"s")
