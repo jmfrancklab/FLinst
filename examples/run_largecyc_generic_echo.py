612b41574b362407daf4aad3a064629aa11d065e
John Franck <jmfranck@jmfDebian.syr.edu>
2024-08-21 11:18:49 -0400

    JF review

--- examples/run_largecyc_generic_echo.py
+++ examples/run_largecyc_generic_echo.py
@@ -137,7 +137,7 @@ data.chunk(
 )
 data.setaxis("ph1", ph1 / 4).setaxis("ph_overall", ph_overall / 4)
 data.set_units("t2", "s")
-data.set_prop("postproc_type", "spincore_diffph_SE_v3")
+data.set_prop("postproc_type", "spincore_diffph_SE_v4")
 data.set_prop("coherence_pathway", {"ph_overall": -1, "ph1": +1})
 data.set_prop("acq_params", config_dict.asdict())
 config_dict = save_data(data, my_exp_type, config_dict, "echo")
