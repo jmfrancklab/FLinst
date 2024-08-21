51f37238dafe02257f89008f4026cfb8becf992a
Alex Guinness <aguinnes@syr.edu>
2024-08-21 13:54:45 -0400

    unneeded changes

--- SpinCore_pp/ppg/IR.py
+++ SpinCore_pp/ppg/IR.py
@@ -19,17 +19,25 @@ import logging
 
 # {{{IR ppg
 def run_IR(
-    settings,
+    nScans,
     vd,
     indirect_idx,
     indirect_len,
+    adcOffset,
+    carrierFreq_MHz,
     nPoints,
-    plen,
+    nEchoes,
+    p90_us,
+    repetition_us,
+    tau_us,
+    SW_kHz,
     indirect_fields=None,
     ph1_cyc=r_[0, 2],
     ph2_cyc=r_[0, 2],
     ret_data=None,
-    plen_as_beta=True,
+    deadtime_us=10.0,
+    deblank_us=1.0,
+    amplitude=1.0,
 ):
     """
     Run a single (signal averaged) scan out of an inversion recovery and generate a single nddata with a vd dimension.
@@ -37,27 +45,33 @@ def run_IR(
 
     Parameters
     ==========
-    settings: configuration
-                    contains the following keys.
-
-                    :nScans: int
-                    :adcOffset: int
-                    :carrierFreq_MHz: float
-                    :nEchoes: int
-                    :repetition_us: float
-                    :tau_us: float
-                    :SW_kHz: float
+    nScans: int
+        number of repeats of the pulse sequence (for averaging over data)
     vd: The variable delay to use for this scan
     indirect_idx: int
         index along the 'indirect' dimension
     indirect_len: int
         size of indirect axis.
         Used to allocate space for the data once the first scan is run.
+    adcOffset: int
+        offset of ADC acquired with SpinCore_apps/C_examples/adc_offset.exe
+    carrierFreq_MHz: float
+            carrier frequency to be set in MHz
     nPoints: int
         number of points for the data
-    plen: float
-        desired length of the pulse -- either μs or s√W
-        (see plen_as_beta)
+    nEchoes: int
+        Number of Echoes to be acquired.
+        This should always be 1, since this pulse
+        program doesn't generate multiple echos.
+    p90_us: float
+        90 time of the probe in us
+    repetition_us: float
+        3-5 x T1 of the sample in seconds
+    tau_us: float
+        Echo Time should be a few ms for a good hermitian function to be
+        applied later in processing. Standard tau_us = 3500.
+    SW_kHz: float
+        spectral width of the data. Minimum = 1.9
     indirect_fields: tuple (pair) of str or (default) None
         Name for the first field of the structured array
         that stores the indirect dimension coordinates.
@@ -73,19 +87,14 @@ def run_IR(
     ph2_cyc: array
         phase steps for the second pulse
     ret_data: nddata (default None)
-        returned data from previous run or `None` for the first run.
-    plen_as_beta: boolean
-        Is plen supplied as a β value [s√W] or directly as programmed length [μs]
-    """
-    assert settings["nEchoes"] == 1, "you must only choose nEchoes=1"
+        returned data from previous run or `None` for the first run."""
+    assert nEchoes == 1, "you must only choose nEchoes=1"
     # take the desired p90 and p180
     # (2*desired_p90) and convert to what needs to
     # be programmed in order to get the desired
     # times
-    prog_p90_us = prog_plen(plen, settings) if plen_as_beta else plen
-    prog_p180_us = (
-        prog_plen(2 * plen, settings) if plen_as_beta else (2 * plen)
-    )
+    prog_p90_us = prog_plen(p90_us)
+    prog_p180_us = prog_plen(2 * p90_us)
     tx_phases = r_[0.0, 90.0, 180.0, 270.0]
     nPhaseSteps = len(ph1_cyc) * len(ph2_cyc)
     data_length = 2 * nPoints * nEchoes * nPhaseSteps
@@ -93,13 +102,7 @@ def run_IR(
     for nScans_idx in range(nScans):
         run_scans_time_list = [time.time()]
         run_scans_names = ["configure"]
-        configureTX(
-            settings["adc_offset"],
-            settings["carrierFreq_MHz"],
-            tx_phases,
-            settings["amplitude"],
-            nPoints,
-        )
+        configureTX(adcOffset, carrierFreq_MHz, tx_phases, amplitude, nPoints)
         run_scans_time_list.append(time.time())
         run_scans_names.append("configure Rx")
         acq_time_ms = configureRX(SW_kHz, nPoints, RX_nScans, nEchoes, nPhaseSteps)
@@ -111,17 +114,17 @@ def run_IR(
         spincore_load(
             [
                 ("phase_reset", 1),
-                ("delay_TTL", settings["deblank_us"]),
+                ("delay_TTL", deblank_us),
                 ("pulse_TTL", prog_p180_us, "ph1", ph1_cyc),
                 ("delay", vd),
                 ("delay_TTL", 1.0),
                 ("pulse_TTL", prog_p90_us, "ph2", ph2_cyc),
-                ("delay", settings["tau_us"]),
-                ("delay_TTL", settings["deblank_us"]),
+                ("delay", tau_us),
+                ("delay_TTL", deblank_us),
                 ("pulse_TTL", prog_p180_us, 0),
-                ("delay", settings["deadtime_us"]),
+                ("delay", deadtime_us),
                 ("acquire", acq_time_ms),
-                ("delay", settings["repetition_us"]),
+                ("delay", repetition_us),
             ]
         )
         run_scans_time_list.append(time.time())
@@ -132,9 +135,7 @@ def run_IR(
         runBoard()
         run_scans_time_list.append(time.time())
         run_scans_names.append("get data")
-        raw_data = getData(
-            data_length, nPoints, settings["nEchoes"], nPhaseSteps
-        )
+        raw_data = getData(data_length, nPoints, nEchoes, nPhaseSteps)
         run_scans_time_list.append(time.time())
         run_scans_names.append("shape data")
         data_array = []
@@ -153,14 +154,14 @@ def run_IR(
                 )
                 # }}}
             mytimes = np.zeros(indirect_len, dtype=times_dtype)
-            time_axis = r_[0:dataPoints] / (settings["SW_kHz"] * 1e3)
+            time_axis = r_[0:dataPoints] / (SW_kHz * 1e3)
             ret_data = psp.ndshape(
-                [indirect_len, settings["nScans"], len(time_axis)],
+                [indirect_len, nScans, len(time_axis)],
                 ["indirect", "nScans", "t"],
             ).alloc(dtype=np.complex128)
             ret_data.setaxis("indirect", mytimes)
             ret_data.setaxis("t", time_axis).set_units("t", "s")
-            ret_data.setaxis("nScans", r_[0 : settings["nScans"]])
+            ret_data.setaxis("nScans", r_[0:nScans])
         elif indirect_idx == 0 and nScans_idx == 0:
             raise ValueError(
                 "you seem to be on the first scan, but ret_data is not None -- it is "
