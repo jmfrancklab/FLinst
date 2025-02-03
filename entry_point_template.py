b8b97f099d5d233372eb9d502c84cafc786d720e
John Franck <jmfranck@jmfDebian.syr.edu>
2025-02-03 16:58:19 -0500

    entry point templates should be good now

--- entry_point_template.py
+++ entry_point_template.py
@@ -1,19 +1,7 @@
+#!/usr/bin/python3
+
 import sys
-from Instruments import power_control_server, just_quit, cmd
-from SpinCore_pp import calc_vdlist
+from @module@ import @function@
 
-def main():
-    script_name = sys.argv[0]
-    if "power_control_server" in script_name:
-        power_control_server.main()
-    elif "quit_power_control" in script_name:
-        just_quit.main()
-    elif "FLInst" in script_name:
-        cmd.cmd()
-    elif "calc_tempol_vd" in script_name:
-        calc_vdlist.print_tempo_vdlist()
-    else:
-        print(f"Unknown command: {script_name}")
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    sys.exit(@function@())
