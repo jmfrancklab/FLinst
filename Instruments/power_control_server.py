47f3fa90ae775541e71539e1d9f68f81056890c3
atahangarip <ag2639@cornell.edu>
2025-12-17 13:44:48 -0500

    "Debugging"

--- Instruments/power_control_server.py
+++ Instruments/power_control_server.py
@@ -1,8 +1,16 @@
 # To be run from the computer connected to the EPR spectrometer
 import time, socket, pickle
-from Instruments import Bridge12, prologix_connection, gigatronics, logobj, LakeShore475, genesys
+from Instruments import (
+    Bridge12,
+    prologix_connection,
+    gigatronics,
+    logobj,
+    LakeShore475,
+    genesys,
+)
 import SpinCore_pp
 import logging
+import numpy as np
 
 IP = "0.0.0.0"
 PORT = 6002
@@ -14,7 +22,9 @@ def main():
             with gigatronics(prologix_instance=p, address=7) as g:
                 with Bridge12() as b:
                     with LakeShore475(p) as h:
-                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM
+                        )
                         sock.bind((IP, PORT))
                         this_logobj = logobj()
                         config_dict = SpinCore_pp.configuration("active.ini")
@@ -40,7 +50,9 @@ def main():
                                     )
                                     b.set_freq(min_f)
                                     min_f = float(b.freq_int()) * 1e3
-                                    conn.send(("%0.6f" % min_f).encode("ASCII"))
+                                    conn.send(
+                                        ("%0.6f" % min_f).encode("ASCII")
+                                    )
                                 else:
                                     raise ValueError(
                                         "I don't understand this 3 component command"
@@ -60,14 +72,23 @@ def main():
                                             else:
                                                 break
                                         nsecs += time.time()
-                                        print("took", j, "tries and", nsecs, "seconds")
+                                        print(
+                                            "took",
+                                            j,
+                                            "tries and",
+                                            nsecs,
+                                            "seconds",
+                                        )
                                         while dBm_setting > last_power + 3:
                                             last_power += 3
                                             nsecs = -1 * time.time()
                                             print("SETTING TO...", last_power)
                                             b.set_power(last_power)
                                             for j in range(30):
-                                                if b.power_float() < last_power:
+                                                if (
+                                                    b.power_float()
+                                                    < last_power
+                                                ):
                                                     time.sleep(0.1)
                                                 else:
                                                     break
@@ -88,14 +109,32 @@ def main():
                                         else:
                                             break
                                     nsecs += time.time()
-                                    print("took", j, "tries and", nsecs, "seconds")
+                                    print(
+                                        "took",
+                                        j,
+                                        "tries and",
+                                        nsecs,
+                                        "seconds",
+                                    )
                                 elif args[0] == b"SET_FIELD":
-                                    B0_des_G = (config_dict["carrierFreq_MHz"]) / config_dict["gamma_eff_mhz_g"])  # B in G
-                                    I_setting = B0_des_G * config_dict["current_v_field_A_G"]
-                                    ramp_dt = 0.05  #Settling time at each ramping step (s)
-                                    ramp_steps = I_setting*2
-                                    myinput = input(strm("Your field is:", B0_des_G, "\nDoes this look okay?"))
-                                    settle_initial_s = 60 
+                                    B0_des_G = (
+                                        config_dict["carrierFreq_MHz"]
+                                        / config_dict["gamma_eff_mhz_g"]
+                                    )  # B in G
+                                    I_setting = (
+                                        B0_des_G
+                                        * config_dict["current_v_field_A_G"]
+                                    )
+                                    ramp_dt = 0.05  # Settling time at each ramping step (s)
+                                    ramp_steps = I_setting * 2
+                                    myinput = input(
+                                        str(
+                                            "Your field is:",
+                                            B0_des_G,
+                                            "\nDoes this look okay?",
+                                        )
+                                    )
+                                    settle_initial_s = 60
                                     if myinput.lower().startswith("n"):
                                         raise ValueError("You said no!!!")
                                     try:
@@ -103,10 +142,14 @@ def main():
                                         g.output = True
                                         print("The power supply is on.")
                                     except:
-                                        raise TypeError("The power supply is not connected.")
+                                        raise TypeError(
+                                            "The power supply is not connected."
+                                        )
                                     # {{{Ramping up the field
                                     print("Ramping up the field")
-                                    for I in np.linspace(0.0, I_setting, ramp_steps):
+                                    for I in np.linspace(
+                                        0.0, I_setting, ramp_steps
+                                    ):
                                         gen.I_limit = I
                                         time.sleep(ramp_dt)
                                     # }}}
@@ -114,28 +157,44 @@ def main():
                                     #     just once at the beginning
                                     true_B0_G = h.field.to("T").magnitude * 1e4
                                     print(
-                                        "adjusting current_v_field_A_G from", config_dict["current_v_field_A_G"]
+                                        "adjusting current_v_field_A_G from",
+                                        config_dict["current_v_field_A_G"],
                                     )
-                                    config_dict["current_v_field_A_G"] *= B0_G / true_B0_G
-                                    print("to", config_dict["current_v_field_A_G"], "and settling again")
+                                    config_dict["current_v_field_A_G"] *= (
+                                        B0_des_G / true_B0_G
+                                    )
+                                    print(
+                                        "to",
+                                        config_dict["current_v_field_A_G"],
+                                        "and settling again",
+                                    )
                                     time.sleep(settle_initial_s)
-                                    I_setting = B0_des_G * config_dict["current_v_field_A_G"]
+                                    I_setting = (
+                                        B0_des_G
+                                        * config_dict["current_v_field_A_G"]
+                                    )
                                     gen.I_limit = I_setting
-                                    print(f"Your field is {h.field.to("T").magnitude * 1e4} G")
+                                    print(
+                                        f"Your field is {h.field.to('T').magnitude * 1e4} G"
+                                    )
                                     logging.info(
-                                    "The ratio of the field I want to the one I"
-                                    f"get is {B0_des_G / true_B0_G}\n"
-                                    "In other words, the discrepancy is"
-                                    f"{true_B0_G - B0_des_G} G"
+                                        "The ratio of the field I want to the one I"
+                                        f"get is {B0_des_G / true_B0_G}\n"
+                                        "In other words, the discrepancy is"
+                                        f"{true_B0_G - B0_des_G} G"
                                     )
 
                                 elif args[0] == b"GET_FIELD":
                                     try:
-                                        print(f"Your field is {h.field.to("T").magnitude * 1e4} G")
+                                        print(
+                                            f"Your field is {h.field.to('T').magnitude * 1e4} G"
+                                        )
                                     except:
-                                        raise ValueError("Hall probe is not connected",
-                                                         "or the power supply is off")
-                                        
+                                        raise ValueError(
+                                            "Hall probe is not connected",
+                                            "or the power supply is off",
+                                        )
+
                                 else:
                                     raise ValueError(
                                         "I don't understand this 2 component command:"
@@ -149,7 +208,9 @@ def main():
                                     conn.close()
                                 elif args[0] == b"GET_POWER":
                                     result = b.power_float()
-                                    conn.send(("%0.1f" % result).encode("ASCII"))
+                                    conn.send(
+                                        ("%0.1f" % result).encode("ASCII")
+                                    )
                                 elif args[0] == b"MW_OFF":
                                     b.soft_shutdown()
 
@@ -163,7 +224,8 @@ def main():
                                 elif args[0] == b"STOP_LOG":
                                     this_logobj.currently_logging = False
                                     retval = (
-                                        pickle.dumps(this_logobj) + b"ENDTCPIPBLOCK"
+                                        pickle.dumps(this_logobj)
+                                        + b"ENDTCPIPBLOCK"
                                     )
                                     conn.send(retval)
                                     this_logobj.reset()
@@ -204,10 +266,16 @@ def main():
                                     if len(data) > 0:
                                         for cmd in data.strip().split(b"\n"):
                                             timelist.append(time.time())
-                                            timelabels.append("about to process")
-                                            leave_open = process_cmd(cmd, this_logobj)
+                                            timelabels.append(
+                                                "about to process"
+                                            )
+                                            leave_open = process_cmd(
+                                                cmd, this_logobj
+                                            )
                                             timelist.append(time.time())
-                                            timelabels.append("processed %s" % cmd)
+                                            timelabels.append(
+                                                "processed %s" % cmd
+                                            )
                                     else:
                                         print("no data received")
                                         timelist.append(time.time())
@@ -218,8 +286,13 @@ def main():
                                             [
                                                 timelabels[j]
                                                 + " --> "
-                                                + str(timelist[j + 1] - timelist[j])
-                                                for j in range(len(timelist) - 1)
+                                                + str(
+                                                    timelist[j + 1]
+                                                    - timelist[j]
+                                                )
+                                                for j in range(
+                                                    len(timelist) - 1
+                                                )
                                             ]
                                             + [timelabels[-1]]
                                         )
@@ -229,5 +302,4 @@ def main():
                                         this_logobj.add(
                                             Rx=b.rxpowerdbm_float(),
                                             power=g.read_power(),
-                                )
-                                        
+                                        )
