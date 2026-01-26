b204f5478d47b7929c5db82f2595d53a8b90a575
John Franck <jmfranck@jmfDebian.syr.edu>
2026-01-26 12:33:07 -0500

    Revert "revert changes to combined ODNP to simplify PR"
    This reverts commit 04fee15f525b2ca41ecd358a529461bf4db6c88a.

--- examples/combined_ODNP.py
+++ examples/combined_ODNP.py
@@ -31,6 +31,88 @@ from SpinCore_pp.ppg import run_spin_echo, run_IR
 from Instruments import power_control
 from datetime import datetime
 
+
+def IR_measurement(
+    vd_list_us,
+    nPoints,
+    config_dict,
+    IR_ph1_cyc,
+    IR_ph2_cyc,
+    FIR_rep,
+    IR_postproc,
+    IR_pathway,
+    phase_cycling,
+    T1_node_names,
+    node_index,
+    target_directory,
+    filename,
+    final_log,
+):
+    ini_time = time.time()
+    vd_data = None
+    for vd_idx, vd in enumerate(vd_list_us):
+        # call B to run_IR
+        vd_data = run_IR(
+            nPoints=nPoints,
+            nEchoes=config_dict["nEchoes"],
+            indirect_idx=vd_idx,
+            indirect_len=len(vd_list_us),
+            ph1_cyc=IR_ph1_cyc,
+            ph2_cyc=IR_ph2_cyc,
+            amplitude=config_dict["amplitude"],
+            vd=vd,
+            plen=config_dict["beta_90_s_sqrtW"],
+            deblank_us=config_dict["deblank_us"],
+            nScans=config_dict["nScans"],
+            adcOffset=config_dict["adc_offset"],
+            carrierFreq_MHz=config_dict["carrierFreq_MHz"],
+            tau_us=config_dict["tau_us"],
+            repetition_us=FIR_rep,
+            SW_kHz=config_dict["SW_kHz"],
+            ret_data=vd_data,
+        )
+    vd_data.set_prop("start_time", ini_time)
+    vd_data.set_prop("stop_time", time.time())
+    vd_data.set_prop("acq_params", config_dict.asdict())
+    vd_data.set_prop("postproc_type", IR_postproc)
+    vd_data.set_prop("coherence_pathway", IR_pathway)
+    vd_data.rename("indirect", "vd")
+    vd_data.setaxis("vd", vd_list_us * 1e-6).set_units("vd", "s")
+    if phase_cycling:
+        vd_data.chunk(
+            "t",
+            ["ph2", "ph1", "t2"],
+            [len(IR_ph2_cyc), len(IR_ph1_cyc), -1],
+        )
+        vd_data.setaxis("ph1", IR_ph1_cyc / 4)
+        vd_data.setaxis("ph2", IR_ph2_cyc / 4)
+    else:
+        vd_data.rename("t", "t2")
+    vd_data.set_units("t2", "s")
+    vd_data.setaxis("nScans", r_[0 : config_dict["nScans"]])
+    vd_data.name(T1_node_names[node_index])
+    nodename = vd_data.name()
+    with h5py.File(
+        os.path.normpath(os.path.join(target_directory, filename))
+    ) as fp:
+        tempcounter = 1
+        orig_nodename = nodename
+        while nodename in fp.keys():
+            nodename = "%s_temp_%d" % (orig_nodename, tempcounter)
+            final_log.append(
+                "this nodename already exists, so I will call it          "
+                "          {nodename}"
+            )
+            vd_data.name(nodename)
+            tempcounter += 1
+    # hdf5_write should be outside the h5py.File with block, since it opens
+    # the file itself
+    vd_data.hdf5_write(filename, directory=target_directory)
+    print("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
+    print(("Name of saved data", vd_data.name()))
+    return vd_data
+
+
 final_log = []
 
 logger = psd.init_logging(level="debug")
@@ -105,10 +187,14 @@ T1_powers_dB = gen_powerlist(
     three_down=False,
 )
 T1_node_names = ["FIR_%ddBm" % j for j in T1_powers_dB]
-logger.info("dB_settings", dB_settings)
-logger.info("correspond to powers in Watts", 10 ** (dB_settings / 10.0 - 3))
-logger.info("T1_powers_dB", T1_powers_dB)
-logger.info("correspond to powers in Watts", 10 ** (T1_powers_dB / 10.0 - 3))
+logger.info(strm("dB_settings", dB_settings))
+logger.info(
+    strm("correspond to powers in Watts", 10 ** (dB_settings / 10.0 - 3))
+)
+logger.info(strm("T1_powers_dB", T1_powers_dB))
+logger.info(
+    strm("correspond to powers in Watts", 10 ** (T1_powers_dB / 10.0 - 3))
+)
 myinput = input("Look ok?")
 if myinput.lower().startswith("n"):
     raise ValueError("you said no!!!")
@@ -143,142 +229,14 @@ if os.path.exists(filename):
         "the file %s already exists, so I'm not going to let you proceed!"
         % filename
     )
-input(
-    "B12 needs to be unplugged and turned off for the thermal! Don't have the "
-    "   power server running just yet"
-)
 # }}}
-# {{{Collect Thermals - serves as a control to compare the thermal of Ep to
-#    ensure no microwaves were leaking
+# {{{
+# THIS PART WAS REMOVED AND WILL BE CHECKED SEPERATELY
+# Collect Thermals - serves as a control to compare the thermal of Ep to
+# ensure no microwaves were leaking
 # call A to run spin echo
-control_thermal = run_spin_echo(
-    nScans=config_dict["thermal_nScans"],
-    indirect_idx=0,
-    indirect_len=1,
-    ph1_cyc=Ep_ph1_cyc,
-    amplitude=config_dict["amplitude"],
-    adcOffset=config_dict["adc_offset"],
-    carrierFreq_MHz=config_dict["carrierFreq_MHz"],
-    nPoints=nPoints,
-    nEchoes=config_dict["nEchoes"],
-    plen=config_dict["beta_90_s_sqrtW"],
-    deblank_us=config_dict["deblank_us"],
-    repetition_us=config_dict["repetition_us"],
-    tau_us=config_dict["tau_us"],
-    SW_kHz=config_dict["SW_kHz"],
-    ret_data=None,
-)
-if config_dict["thermal_nScans"] > 1:
-    control_thermal.setaxis("nScans", r_[0 : config_dict["thermal_nScans"]])
-if phase_cycling:
-    control_thermal.chunk("t", ["ph1", "t2"], [len(Ep_ph1_cyc), -1])
-    control_thermal.setaxis("ph1", Ep_ph1_cyc / 4)
-    control_thermal.reorder(["ph1", "nScans", "t2"])
-else:
-    control_thermal.rename("t", "t2")
-control_thermal.set_units("t2", "s")
-control_thermal.name("control_thermal")
-control_thermal.set_prop("postproc_type", Ep_postproc)
-control_thermal.set_prop("acq_params", config_dict.asdict())
-control_thermal.name("control_thermal")
-control_thermal.set_prop("coherence_pathway", {"ph1": 1})
-nodename = control_thermal.name()
-# {{{ on first write, if we can't access the directory, write to a temp file
-try:
-    control_thermal.hdf5_write(filename, directory=target_directory)
-except Exception:
-    final_log.append(
-        f"I had problems writing to the correct file {filename}, so I'm going "
-        "       to try to save your file to temp_ctrl.h5 in the current"
-        " directory"
-    )
-    if os.path.exists("temp_ctrl.h5"):
-        final_log.append("There is already a temp_ctrl.h5 -- I'm removing it")
-        os.remove("temp_ctrl.h5")
-        target_directory = os.path.getcwd()
-        filename = "temp_ctrl.h5"
-        control_thermal.hdf5_write(filename, directory=target_directory)
-        final_log.append(
-            "change the name accordingly once this is done running!"
-        )
-# }}}
-logger.info("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
-logger.debug(psd.strm("Name of saved data", control_thermal.name()))
-# }}}
-# {{{IR at no power
-#   this is outside the log, so to deal with this during processing, just check
-#   if the start and stop time are outside the log (greater than last time of
-#   the time axis, or smaller than the first)
-ini_time = time.time()
-vd_data = None
-logger.debug("starting T1s")
-for vd_idx, vd in enumerate(vd_list_us):
-    # call A to run_IR
-    logger.debug(f"T1 #{vd_idx}")
-    vd_data = run_IR(
-        nPoints=nPoints,
-        nEchoes=config_dict["nEchoes"],
-        indirect_idx=vd_idx,
-        indirect_len=len(vd_list_us),
-        ph1_cyc=IR_ph1_cyc,
-        ph2_cyc=IR_ph2_cyc,
-        vd=vd,
-        nScans=config_dict["thermal_nScans"],
-        plen=config_dict["beta_90_s_sqrtW"],
-        deblank_us=config_dict["deblank_us"],
-        adcOffset=config_dict["adc_offset"],
-        carrierFreq_MHz=config_dict["carrierFreq_MHz"],
-        amplitude=config_dict["amplitude"],
-        tau_us=config_dict["tau_us"],
-        repetition_us=FIR_rep,
-        SW_kHz=config_dict["SW_kHz"],
-        ret_data=vd_data,
-    )
-vd_data.rename("indirect", "vd")
-vd_data.setaxis("vd", vd_list_us * 1e-6).set_units("vd", "s")
-if phase_cycling:
-    vd_data.chunk(
-        "t", ["ph2", "ph1", "t2"], [len(IR_ph1_cyc), len(IR_ph2_cyc), -1]
-    )
-    vd_data.setaxis("ph1", IR_ph1_cyc / 4)
-    vd_data.setaxis("ph2", IR_ph2_cyc / 4)
-else:
-    vd_data.rename("t", "t2")
-vd_data.set_units("t2", "s")
-vd_data.setaxis("nScans", r_[0 : config_dict["thermal_nScans"]])
-vd_data.name("FIR_noPower")
-vd_data.set_prop("stop_time", time.time())
-vd_data.set_prop("coherence_pathway", IR_pathway)
-vd_data.set_prop("start_time", ini_time)
-vd_data.set_prop("acq_params", config_dict.asdict())
-vd_data.set_prop("postproc_type", IR_postproc)
-nodename = vd_data.name()
-# {{{ again, implement a file fallback
-with h5py.File(
-    os.path.normpath(os.path.join(target_directory, f"{filename}"))
-) as fp:
-    if nodename in fp.keys():
-        final_log.append(
-            "this nodename already exists, so I will call it temp"
-        )
-        nodename = "temp_noPower"
-        final_log.append(
-            f"I had problems writing to the correct file {filename} so I'm    "
-            "        going to try to save this node as temp_noPower"
-        )
-        vd_data.name(nodename)
-# hdf5_write should be outside the h5py.File with block, since it opens the
-# file itself
-vd_data.hdf5_write(filename, directory=target_directory)
-# }}}
-logger.debug("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
-logger.debug(psd.strm("Name of saved data", vd_data.name()))
 # }}}
 # {{{run enhancement
-input(
-    "Now plug the B12 back in and start up the FLInst power control server so "
-    "we can continue!"
-)
 with power_control() as p:
     # we do not dip lock or anything here, because we assume
     # uw_dip_center_GHz stores the frequency of the center of the cavity
@@ -319,6 +277,25 @@ with power_control() as p:
             time_axis_coords = DNP_data.getaxis("indirect")
         time_axis_coords[j]["start_times"] = DNP_ini_time
         time_axis_coords[j]["stop_times"] = DNP_thermal_done
+
+    # T1 measurement while the mw power is off
+    logger.debug("Starting T1s")
+    IR_measurement(
+        vd_list_us=vd_list_us,
+        nPoints=nPoints,
+        config_dict=config_dict,
+        IR_ph1_cyc=IR_ph1_cyc,
+        IR_ph2_cyc=IR_ph2_cyc,
+        FIR_rep=FIR_rep,
+        IR_postproc=IR_postproc,
+        IR_pathway=IR_pathway,
+        phase_cycling=phase_cycling,
+        T1_node_names=["FIR_noPower"],
+        node_index=0,
+        target_directory=target_directory,
+        filename=filename,
+        final_log=final_log,
+    )
     power_settings_dBm = zeros_like(dB_settings)
     time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     for j, this_dB in enumerate(dB_settings):
@@ -436,68 +413,22 @@ with power_control() as p:
             raise ValueError("After 10 tries, the power has still not settled")
         time.sleep(5)
         meter_power = p.get_power_setting()
-        ini_time = time.time()
-        vd_data = None
-        for vd_idx, vd in enumerate(vd_list_us):
-            # call B to run_IR
-            vd_data = run_IR(
-                nPoints=nPoints,
-                nEchoes=config_dict["nEchoes"],
-                indirect_idx=vd_idx,
-                indirect_len=len(vd_list_us),
-                ph1_cyc=IR_ph1_cyc,
-                ph2_cyc=IR_ph2_cyc,
-                amplitude=config_dict["amplitude"],
-                vd=vd,
-                plen=config_dict["beta_90_s_sqrtW"],
-                deblank_us=config_dict["deblank_us"],
-                nScans=config_dict["nScans"],
-                adcOffset=config_dict["adc_offset"],
-                carrierFreq_MHz=config_dict["carrierFreq_MHz"],
-                tau_us=config_dict["tau_us"],
-                repetition_us=FIR_rep,
-                SW_kHz=config_dict["SW_kHz"],
-                ret_data=vd_data,
-            )
-        vd_data.set_prop("start_time", ini_time)
-        vd_data.set_prop("stop_time", time.time())
-        vd_data.set_prop("acq_params", config_dict.asdict())
-        vd_data.set_prop("postproc_type", IR_postproc)
-        vd_data.set_prop("coherence_pathway", IR_pathway)
-        vd_data.rename("indirect", "vd")
-        vd_data.setaxis("vd", vd_list_us * 1e-6).set_units("vd", "s")
-        if phase_cycling:
-            vd_data.chunk(
-                "t",
-                ["ph2", "ph1", "t2"],
-                [len(IR_ph2_cyc), len(IR_ph1_cyc), -1],
-            )
-            vd_data.setaxis("ph1", IR_ph1_cyc / 4)
-            vd_data.setaxis("ph2", IR_ph2_cyc / 4)
-        else:
-            vd_data.rename("t", "t2")
-        vd_data.set_units("t2", "s")
-        vd_data.setaxis("nScans", r_[0 : config_dict["nScans"]])
-        vd_data.name(T1_node_names[j])
-        nodename = vd_data.name()
-        with h5py.File(
-            os.path.normpath(os.path.join(target_directory, filename))
-        ) as fp:
-            tempcounter = 1
-            orig_nodename = nodename
-            while nodename in fp.keys():
-                nodename = "%s_temp_%d" % (orig_nodename, tempcounter)
-                final_log.append(
-                    "this nodename already exists, so I will call it          "
-                    "          {nodename}"
-                )
-                vd_data.name(nodename)
-                tempcounter += 1
-        # hdf5_write should be outside the h5py.File with block, since it opens
-        # the file itself
-        vd_data.hdf5_write(filename, directory=target_directory)
-        print("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
-        print(("Name of saved data", vd_data.name()))
+        IR_measurement(
+            vd_list_us=vd_list_us,
+            nPoints=nPoints,
+            config_dict=config_dict,
+            IR_ph1_cyc=IR_ph1_cyc,
+            IR_ph2_cyc=IR_ph2_cyc,
+            FIR_rep=FIR_rep,
+            IR_postproc=IR_postproc,
+            IR_pathway=IR_pathway,
+            phase_cycling=phase_cycling,
+            T1_node_names=T1_node_names,
+            node_index=j,
+            target_directory=target_directory,
+            filename=filename,
+            final_log=final_log,
+        )
     this_log = p.stop_log()
 # }}}
 config_dict.write()
