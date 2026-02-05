e02eb2bd896e59c70afa86c8a46830bbb3fcb060
John Franck <jmfranck@jmfDebian.syr.edu>
2026-02-05 13:25:37 -0500

    move changes from old inst_notebook repo

--- Instruments/HP6623A.py
+++ Instruments/HP6623A.py
@@ -1,6 +1,47 @@
 from pylab import *
 from .gpib_eth import gpib_eth
 from .log_inst import logger
+import time
+
+class ChannelAware(object):
+    """Build a class that we can use to decorate -- similar to @property, except that the decorated object can have many channels:
+
+    self.voltage[0] = 3 # set the voltage of channel 0 to 3
+    print(self.voltage) # print the voltage of channel 3
+    """
+    def num_channels(self,*args):
+        raise ValueError("you need to define num_channels")
+    def channel_set_func(self,channel,val):
+        raise ValueError("you need to define channel_set_func")
+    def channel_get_func(self,channel):
+        raise ValueError("you need to define channel_get_func")
+    def set_num_channels(self,number):
+        def ret_num_channels():
+            return number
+        self.num_channels = ret_num_channels
+    def setter(self,setter_func):
+        "if called as a decorator, define the set function"
+        self.channel_set_func = setter_func
+        return
+    def __init__(self,getter_func):
+        """The name function that is decorated becomes
+        the instance, and the decorated function should
+        accept one argument -- the channel -- and contain code on how to
+        retrieve the relevant info for the given
+        channel"""
+        self.channel_get_func = getter_func
+        return
+    def __getitem__(self,channel,val):
+        return self.channel_get_func(channel,val)
+    def __getslice__(self,*args):
+        raise ValueError("we probably could define slices, but that's not implemented yet")
+    def __setitem__(self,channel,val):
+        self.channel_set_func(channel,val)
+    def __len__(self):
+        return self.num_channels()
+    def __iter__(self):
+        for thischannel in range(self.num_channels()):
+            yield self.channel_get_func(thischannel)
 
 class HP6623A (gpib_eth):
     def __init__(self, prologix_instance=None, address=None):
@@ -34,6 +75,10 @@ class HP6623A (gpib_eth):
         
         """
         self.write("VSET %s,%s"%(str(ch),str(val)))
+        if val == 0.0:
+            return
+        else:
+            time.sleep(5)
         return
     def get_voltage(self, ch):
         r"""get voltage (in Volts) on specific channel
@@ -77,7 +122,15 @@ class HP6623A (gpib_eth):
         
         """
         self.write("IOUT? %s"%str(ch))
-        return float(self.read())
+        curr_reading = float(self.read())
+        for i in range(30):
+            self.write("IOUT? %s"%str(ch))
+            this_val = float(self.read())
+            if curr_reading == this_val:
+                break
+            if i > 28:
+                print("Not able to get stable meter reading after 30 tries. Returning: %0.3f"%curr_reading)
+        return curr_reading 
     def output(self, ch, trigger):
         r"""turn on or off the output on specific channel
 
@@ -99,8 +152,8 @@ class HP6623A (gpib_eth):
             trigger = 1
         elif not trigger:
             trigger = 0
-        return self.write("OUT %s,%s"%(str(ch),str(trigger)))
         self.write("OUT %s,%s"%(str(ch),str(trigger)))
+        return 
     def check_output(self, ch):
         r"""check the output status of a specific channel
 
@@ -121,4 +174,10 @@ class HP6623A (gpib_eth):
             print("Ch %s output is ON"%ch)
         return 
     def close(self):
+        for i in [1,2,3]:
+            # set voltage and current to 0 and turn off output on all channels,
+            # before exiting
+            self.set_voltage(i,0)
+            self.set_current(i,0)
+            self.output(i,False)
         super().close()
