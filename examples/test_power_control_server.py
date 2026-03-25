95004ad0d5904406aa223b64afb850a62e194886
atahangarip <ag2639@cornell.edu>
2026-03-25 17:54:55 -0400

    test code

--- examples/test_power_control_server.py
+++ examples/test_power_control_server.py
@@ -1,7 +1,7 @@
 """test the power control server
 
 run this while running `python -m Instruments.power_control_server`
-(or `power_control_server` as a command)
+(or `FLInst server` as a command)
 on the same computer
 
 generates hdf output to be read by test_power_control_server_read.py"""
@@ -23,9 +23,9 @@ assert not os.path.exists("output.h5"), (
     " we're writing a fresh h5 file"
 )
 with power_control() as p:
+    p.set_field(1000)
     p.set_power(10)
     p.set_freq(config_dict["uw_dip_center_GHz"] * 1e9)
-    input("press enter once the waveguide has switched")
     # {{{ run a loop that should take about 50s + execution time.  Step through
     #     3 powers -- unmodified (0 dB?), 10.5 dBm and 12 dBm
     for j in range(100):
@@ -37,9 +37,11 @@ with power_control() as p:
         elif j == 30:
             logger.info("set first power")
             p.set_power(10.5)
+            p.set_field(1200)
         elif j == 60:
             logger.info("set second power")
             p.set_power(12)
+            p.set_field(1500)
     this_log = p.stop_log()
     # }}}
     # p.arrange_quit()
