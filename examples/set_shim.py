f34c1f1b90da4fbdb6b3ca0771cbacf43330e39d
atahangarip <ag2639@cornell.edu>
2026-03-27 15:39:13 -0400

    set shim update

--- examples/set_shim.py
+++ examples/set_shim.py
@@ -4,20 +4,24 @@ from SpinCore_pp import configuration
 
 config_dict = configuration("active.ini")
 with power_control() as p:
-    if len(sys.argv) == 1:
-        shim_current_A = config_dict["shim_y_current_A"]
+    if len(sys.argv) == 2:
+        shim_name = str(sys.argv[1])
+        channel = config_dict["shim_channels"][shim_name][1]
+        shim_voltage_V = config_dict["shim_y_voltage_V"]
         print(
-            "Your optial shim current is "
-            f"{config_dict['shim_y_current_A']} "
+            "Your optial shim voltage is "
+            f"{config_dict['shim_y_voltage_V']} "
             "and I am setting the field to "
-            f"{shim_current_A}.\n\n"
+            f"{shim_voltage_V}.\n\n"
             "NOTE: If you want to manually set a particular field "
             "(including 0 to turn off the shim) you can specify "
             "as an argument on the command line)"
         )
-        p.set_shim_current(shim_current_A)
-    elif len(sys.argv) == 2:
-        shim_current_A = float(sys.argv[1])
-        p.set_shim_current(shim_current_A)
+        p.set_shim_voltage(shim_voltage_V)
+    elif len(sys.argv) == 3:
+        shim_name = str(sys.argv[1])
+        channel = config_dict["shim_channels"][shim_name][1]
+        shim_voltage_V = float(sys.argv[2])
+        p.set_shim_voltage(shim_voltage_V)
     else:
         raise ValueError("You entered an extra argument.")
