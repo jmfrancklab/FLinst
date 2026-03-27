52092261450a6e5e2c6c07caa7fe3e6de05ad1b5
Atahan Garip <ag2639@cornell.edu>
2026-03-27 16:30:04 -0700

    now it changes the date

--- SpinCore_pp/save_data.py
+++ SpinCore_pp/save_data.py
@@ -43,6 +43,7 @@ def save_data(dataset, my_exp_type, config_dict, counter_type=None, proc=True):
         for k in config_dict.keys():
             if k.endswith("_counter"):
                 config_dict[k] = 0
+        config_dict["date"] = date_now
     # auto-increment the counter
     config_dict["%s_counter" % counter_type] += 1
     # }}}
