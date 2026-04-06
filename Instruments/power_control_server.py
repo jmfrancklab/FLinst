61f25bd8b30b1fd939033b18e86132bf0737c07a
atahangarip <ag2639@cornell.edu>
2026-04-06 12:24:24 -0400

    power control server logs the field

--- Instruments/power_control_server.py
+++ Instruments/power_control_server.py
@@ -80,6 +80,7 @@ def main():
                 this_logobj.add(
                     Rx=b.rxpowerdbm_float(),
                     power=g.read_power(),
+                    field=h.field_in_G,
                     cmd=cmd,
                 )
             args = cmd.split(b" ")
@@ -322,4 +323,5 @@ def main():
                         this_logobj.add(
                             Rx=b.rxpowerdbm_float(),
                             power=g.read_power(),
+                            field=h.field_in_G,
                         )
