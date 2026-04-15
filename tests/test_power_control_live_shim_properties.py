c9d6a20b15ca33938d3efe77e706ee0132a223e5
John Franck <jmfranck@jmfDebian.syr.edu>
2026-04-15 14:27:10 -0400

    reviewed

--- tests/test_power_control_live_shim_properties.py
+++ tests/test_power_control_live_shim_properties.py
@@ -1,4 +1,3 @@
-# TODO ☐: review not complete in most recent round
 import unittest
 import numpy as np
 from Instruments.power_control import power_control
@@ -29,10 +28,10 @@ class TestPowerControlLiveShimProperties(unittest.TestCase):
 
     def test_named_voltage_getter_matches_live_readback(self):
         with power_control() as p:
-            z0_voltage_V = 1.501
+            z0_voltage_V = p.round_shim_voltage("Z0", 1.501)
             p.shim_voltage[:] = 0.0
             p.shim_current[:] = 1.5
-            p.shim_voltage["Z0"] = p.round_shim_voltage("Z0", z0_voltage_V)
+            p.shim_voltage["Z0"] = z0_voltage_V
             self.assertTrue(np.isclose(p.shim_voltage["Z0"], z0_voltage_V))
 
     def test_slice_voltage_getter_matches_live_readback(self):
@@ -40,15 +39,16 @@ class TestPowerControlLiveShimProperties(unittest.TestCase):
             shim_names = sorted(p.get_shims())
             requested_voltage_V = [2.0] * len(shim_names)
             p.shim_voltage[:] = requested_voltage_V
-            voltage_readback = p.get_shims()
+            shim_readback = p.get_shims()
             np.testing.assert_allclose(
                 p.shim_voltage[:],
-                [voltage_readback[shim_name][0] for shim_name in shim_names],
+                # the voltage is given by the second element of the tuple
+                [shim_readback[shim_name][0] for shim_name in shim_names],
             )
 
     def test_named_current_getter_matches_live_readback(self):
         with power_control() as p:
-            z0_current_A = 0.508
+            z0_current_A = p.round_shim_current("Z0", 0.508)
             p.shim_current[:] = 0.0
             p.shim_voltage[:] = 15.0
             p.shim_current["Z0"] = z0_current_A
@@ -60,10 +60,11 @@ class TestPowerControlLiveShimProperties(unittest.TestCase):
             shim_names = sorted(p.get_shims())
             requested_current_A = [0.508] * len(shim_names)
             p.shim_current[:] = requested_current_A
-            current_readback = p.get_shims()
+            shim_readback = p.get_shims()
             np.testing.assert_allclose(
                 p.shim_current[:],
-                [current_readback[shim_name][1] for shim_name in shim_names],
+                # the current is given by the second element of the tuple
+                [shim_readback[shim_name][1] for shim_name in shim_names],
             )
 
     def test_round_shim_voltage_accepts_list_and_array(self):
